b2129ea6360206f60b6b5b28ff6866c3a014c059
ParadkarPrasad <prasadparad37@gmail.com>
2024-09-04 12:42:22 +0100

    Refractoring redux app

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import NewNotes from "./NewNotes"
+import Notes from "./Notes"
+const App = () => {
+  return(
+    <div>
+      <NewNotes />
+      <Notes />
+    </div>
+  )
+  
+}
+
+export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,61 +1,17 @@
-import noteReducer from './reducers/noteReducer'
 import ReactDOM from 'react-dom/client'
 import { createStore } from 'redux'
-import { createRoot } from 'react-dom/client'
 
-import './index.css'
-// const noteReducer = (state = [], action) => {
-//   if (action.type === 'NEW_NOTE') {
-//     return state.concat(action.payload)
-//   }
+import { Provider } from 'react-redux'
 
-//   return state
-// }
+import App from './App'
+import noteReducer from './reducers/noteReducer'
 
 const store = createStore(noteReducer)
 
-store.dispatch({
-  type: 'NEW_NOTE',
-  payload: {
-    content: 'the app state is in redux store',
-    important: true,
-    id: 1
-  }
-})
+ReactDOM.createRoot(document.getElementById('root')).render(
 
-store.dispatch({
-  type: 'NEW_NOTE',
-  payload: {
-    content: 'state changes are made with actions',
-    important: false,
-    id: 2
-  }
-})
-
-const App = () => {
-  return(
-    <div>
-      <ul>
-        {store.getState().map(note=>
-          <li key={note.id}>
-            {note.content} <strong>{note.important ? 'important' : ''}</strong>
-          </li>
-        )}
-        </ul>
-    </div>
-  )
-}
-
-const root = ReactDOM.createRoot(document.getElementById('root'))
-
-const renderApp = () => {
-  root.render(<App />)
-}
-
-renderApp()
-store.subscribe(renderApp)
-createRoot(document.getElementById('root')).render(
-  
+  <Provider store={store}>
     <App />
-  
+
+  </Provider>
 )
